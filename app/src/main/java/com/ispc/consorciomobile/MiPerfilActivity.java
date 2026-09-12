99ad6673fe106f81dd02ad7cbdb0543030553eab
gastón_Alonso() <tonga88@live.com.ar>
2026-09-12 14:08:49 -0300

    feat: maquetado visual de MiPerfilActivity y navegación de retroceso

--- app/src/main/java/com/ispc/consorciomobile/MiPerfilActivity.java
+++ app/src/main/java/com/ispc/consorciomobile/MiPerfilActivity.java
@@ -3,6 +3,8 @@ package com.ispc.consorciomobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MiPerfilActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MiPerfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mi_perfil);
+
+        Button btnVolver = findViewById(R.id.btnVolver);
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
