48858b1bc1cb30ecb2263019309c243f35875a55
Ivan Moreno <iv4n.m0reno@gmail.com>
2026-09-13 19:46:57 -0300

    feat: Diseño de la vista comunicados desde el rol residente con ejemplo de historial de comunicados

---
+++ app/src/main/java/com/ispc/consorciomobile/ComunicadosResidenteActivity.java
@@ -0,0 +1,13 @@
+package com.ispc.consorciomobile;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ComunicadosResidenteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comunicados_residente);
+    }
+}
