7e09fd16f5b6c2352caa4a720404b7da73801d04
Vargas-Veronica <vargas.veronica55@gmail.com>
2026-09-13 22:34:11 -0300

    Agrego botón para navegar

--- app/src/main/java/com/ispc/consorciomobile/MisReclamosActivity.java
+++ app/src/main/java/com/ispc/consorciomobile/MisReclamosActivity.java
@@ -2,7 +2,10 @@ package com.ispc.consorciomobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MisReclamosActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MisReclamosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mis_reclamos);
+
+        Button btn_reclamos_volver = findViewById(R.id.btn_reclamos_volver);
+
+        btn_reclamos_volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MisReclamosActivity.this , MainActivity.class
+                        );
+                startActivity(intent);
+            }
+        });
     }
 }
