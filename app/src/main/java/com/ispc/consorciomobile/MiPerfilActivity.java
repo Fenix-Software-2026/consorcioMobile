0511a7e2c6ac153e23e038e9c397210442abed99
gastón_Alonso() <tonga88@live.com.ar>
2026-09-15 17:12:42 -0300

    feat: implementar pantalla Mi Perfil, estilos y ruteo en dashboard

--- app/src/main/java/com/ispc/consorciomobile/MiPerfilActivity.java
+++ app/src/main/java/com/ispc/consorciomobile/MiPerfilActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MiPerfilActivity extends AppCompatActivity {
 
@@ -13,11 +15,31 @@ public class MiPerfilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mi_perfil);
 
-        Button btnVolver = findViewById(R.id.btnVolver);
+        TextView btnVolver = findViewById(R.id.btnVolverBarra);
         btnVolver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        //Btn actualizar contraseña
+        Button btnActualizar = findViewById(R.id.btnActualizarContrasena);
+        btnActualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO:Programar validacion
+                Toast.makeText(MiPerfilActivity.this,"Funcion en desarrollo",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Btn Cerrar Sesion
+        Button btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
+        btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO:Borrar token y volver Login
+                Toast.makeText(MiPerfilActivity.this, "Cerrando Sesión....", Toast.LENGTH_SHORT).show();
             }
         });
     }
