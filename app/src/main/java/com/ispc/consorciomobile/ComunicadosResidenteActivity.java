91c726a620b61fe93510d81637298e9584ad71b9
Ivan Moreno <iv4n.m0reno@gmail.com>
2026-09-15 01:08:09 -0300

    feat: rediseña UI/UX en comunicados sobre la vista residente y se agrega navegacion

--- app/src/main/java/com/ispc/consorciomobile/ComunicadosResidenteActivity.java
+++ app/src/main/java/com/ispc/consorciomobile/ComunicadosResidenteActivity.java
@@ -1,6 +1,8 @@
 package com.ispc.consorciomobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ComunicadosResidenteActivity extends AppCompatActivity {
@@ -9,5 +11,15 @@ public class ComunicadosResidenteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comunicados_residente);
+
+        // Referencia al botón de retorno en la barra superior
+        Button btnVolverDashboard = findViewById(R.id.btnVolverDashboard);
+
+        // Configuración del Intent para volver al Dashboard del Residente
+        btnVolverDashboard.setOnClickListener(v -> {
+            Intent intent = new Intent(ComunicadosResidenteActivity.this, DashboardResidenteActivity.class);
+            startActivity(intent);
+            finish(); // Cierra esta activity opcionalmente para mantener limpia la pila de navegación
+        });
     }
 }
