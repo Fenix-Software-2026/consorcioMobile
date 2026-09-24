e0cdcecd6df56c8d7333df25522aedeeeee64b77
Vargas-Veronica <vargas.veronica55@gmail.com>
2026-09-24 18:56:32 -0300

    feat(ui):Agrego spinner para seleccionar categoria en el form de crear reclamo

--- app/src/main/java/com/ispc/consorciomobile/CrearReclamoActivity.java
+++ app/src/main/java/com/ispc/consorciomobile/CrearReclamoActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class CrearReclamoActivity extends AppCompatActivity {
 
@@ -16,9 +19,39 @@ public class CrearReclamoActivity extends AppCompatActivity {
 
         Button btnCrearReclamo;
         Button btnCancelarReclamo;
+        EditText etTituloCrearReclamo;
+        Spinner spCategoriaCrearReclamo;
+        EditText etDescripcionCrearReclamo;
+
+
+        etTituloCrearReclamo= findViewById(R.id.etTituloCrearReclamo);
+        spCategoriaCrearReclamo = findViewById(R.id.spCategoriaCrearReclamo);
+        etDescripcionCrearReclamo = findViewById(R.id.etDescripcionCrearReclamo);
 
         btnCrearReclamo = findViewById(R.id.btnCrearReclamo);
         btnCancelarReclamo = findViewById(R.id.btnCancelarCrearReclamo);
+
+        String[] categorias = {
+                "Seleccionar categoría",
+                "Plomería",
+                "Electricidad",
+                "Gas",
+                "Humedad / Filtraciones",
+                "Ascensor",
+                "Limpieza",
+                "Seguridad / Accesos",
+                "Ruidos molestos",
+                "Internet / Antena",
+                "Otros"
+        };
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<>(
+                this , androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, categorias
+        );
+
+        adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item);
+
+        spCategoriaCrearReclamo.setAdapter(adapter);
 
         btnCrearReclamo.setOnClickListener(new View.OnClickListener() {
 
